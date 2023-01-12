f5d54cfe980ac4f59f5996989fd7869d11e807ea
Schlabach <matthewshawn89@gmail.com>
2023-01-12 06:15:27 -0500

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string> { "Super Mario World", "Mortal Kombat", "The Legend of Zelda", "Street Fighter", "Donkey Kong Country", "Mario Kart" };
+
+
+            IEnumerable<string> gamesOrdered = videoGames.OrderByDescending(x => x.Length);
+
+            foreach (string game in gamesOrdered)
+            {
+                Console.WriteLine(game);
+            }
+
         }
     }
 }
